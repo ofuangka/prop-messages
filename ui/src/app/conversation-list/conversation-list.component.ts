26557efa93180266b3b54f24760abce72542b6fb
Osha Fuangkasae <ofuangka@gmail.com>
2016-11-27 15:30:54 -0800

    feat: more styling improvements

--- ui/src/app/conversation-list/conversation-list.component.ts
+++ ui/src/app/conversation-list/conversation-list.component.ts
@@ -4,34 +4,37 @@ import { ConversationService } from '../conversation.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'prop-messages-conversation-list',
-  templateUrl: './conversation-list.component.html',
-  styleUrls: ['./conversation-list.component.css']
+	selector: 'prop-messages-conversation-list',
+	templateUrl: './conversation-list.component.html',
+	styleUrls: ['./conversation-list.component.css']
 })
 export class ConversationListComponent implements OnInit {
 
-  conversations: Conversation[];
+	showCreateConversation = false;
 
-  constructor(private conversationService: ConversationService, private router: Router) { }
+	conversations: Conversation[];
 
-  ngOnInit() {
-    this.conversationService.list().then((conversations) => this.conversations = conversations);
-  }
+	constructor(private conversationService: ConversationService, private router: Router) { }
 
-  showCreateConversation() {
+	ngOnInit() {
+		this.conversationService.list().then((conversations) => this.conversations = conversations);
+	}
 
-  }
+	getIconHref(iconId: string) {
+		return iconId;
+	}
 
-  getIconHref(iconId: string) {
-    return iconId;
-  }
+	getSummary(conversation: Conversation) {
+		return 'This is a long summary of a long conversation that may be very long and it will be too long to fit in a single line hopefully I hope that it will just end up working but it probably won\'t';
+	}
 
-  getSummary(conversation: Conversation) {
-    return 'This is a long summary of a long conversation that may be very long and it will be too long to fit in a single line hopefully I hope that it will just end up working but it probably won\'t';
-  }
+	showMessages(conversation: Conversation) {
+		this.router.navigate(['/conversations', conversation.id]);
+	}
 
-  showMessages(conversation: Conversation) {
-    this.router.navigate(['/conversations', conversation.id]);
-  }
+	saveConversation(to, imessage) {
+		console.log(to);
+		console.log(imessage);
+	}
 
 }
--- ui/src/app/messages/messages.component.ts
+++ ui/src/app/messages/messages.component.ts
@@ -1,34 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageGroupService } from '../message-group.service';
 import { MessageGroup } from '../domain/message-group';
-import { ActivatedRoute, Params} from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { ConversationService } from '../conversation.service';
 import { Conversation } from '../domain/conversation';
 
 
 @Component({
-  selector: 'prop-messages-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+	selector: 'prop-messages-messages',
+	templateUrl: './messages.component.html',
+	styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
 
-  conversation: Conversation;
+	conversation: Conversation;
 
-  messageGroups: MessageGroup[] = [];
+	messageGroups: MessageGroup[] = [];
 
-  constructor(
-    private messageGroupService: MessageGroupService, 
-    private route: ActivatedRoute, 
-    private conversationService: ConversationService
-  ) { }
+	constructor(
+		private messageGroupService: MessageGroupService,
+		private route: ActivatedRoute,
+		private conversationService: ConversationService
+	) { }
 
-  ngOnInit() {
-    this.route.params.forEach((params: Params) => {
-      let conversationId = params['conversationId'];
-      this.conversationService.get(conversationId).then((conversation) => this.conversation = conversation);
-      this.messageGroupService.list(conversationId).then((messageGroups) => this.messageGroups = messageGroups);
-    });
-  }
+	ngOnInit() {
+		this.route.params.forEach((params: Params) => {
+			let conversationId = params['conversationId'];
+			this.conversationService.get(conversationId).then((conversation) => this.conversation = conversation);
+			this.messageGroupService.list(conversationId).then((messageGroups) => this.messageGroups = messageGroups);
+		});
+	}
 
 }
